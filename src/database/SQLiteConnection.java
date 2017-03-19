2170c146c285983df0f3bf1fd3fd4ba0907eabe2
Splerge <damnsplurge@gmail.com>
2017-03-19 16:08:59 +1100

    Added database tables for almost every kind of information we will need in both Part A and B

--- src/database/SQLiteConnection.java
+++ src/database/SQLiteConnection.java
@@ -5,6 +5,16 @@ import java.sql.*;
 public class SQLiteConnection {
 	private static Connection conn = null;
 	
+	public static void createTables() {
+		createUsersTable();
+		createBusinessTable();
+		createOwnerTable();
+		createEmployeeTable();
+		createAvailabilitiesTable();
+		createEmployeeWorkingTimesTable();
+		createBookingsTable();		
+	}
+	
 	public static Connection getDBConnection() { // connects to a database only once, stays open after that
 		if (conn == null) {
 			try {
@@ -29,6 +39,79 @@ public class SQLiteConnection {
 					System.out.println(e.getMessage());
 				}
 	}
+	
+	public static void createBusinessTable() {
+		String sql = "CREATE TABLE IF NOT EXISTS Businessinfo (businessname Varchar(255) Primary Key, name Varchar(255), address Varchar(255), mobileno Varchar(255))";
+				try {
+					Connection c = getDBConnection();
+					Statement stmt = c.createStatement();
+			            stmt.execute(sql);
+				}
+				catch(Exception e){
+					System.out.println(e.getMessage());
+				}
+	}
+	
+	public static void createOwnerTable() {
+		String sql = "CREATE TABLE IF NOT EXISTS Ownerinfo (Foreign Key(businessname) references Businessinfo(businessname), username Varchar(255) Primary Key, name Varchar(255), address Varchar(255), mobileno Varchar(255))";
+				try {
+					Connection c = getDBConnection();
+					Statement stmt = c.createStatement();
+			            stmt.execute(sql);
+				}
+				catch(Exception e){
+					System.out.println(e.getMessage());
+				}
+	}
+	
+	public static void createEmployeeTable() {
+		String sql = "CREATE TABLE IF NOT EXISTS Employeeinfo (Foreign Key(businessname) references Businessinfo(businessname), name Varchar(255) Primary Key, address Varchar(255), mobileno Varchar(255))";
+				try {
+					Connection c = getDBConnection();
+					Statement stmt = c.createStatement();
+			            stmt.execute(sql);
+				}
+				catch(Exception e){
+					System.out.println(e.getMessage());
+				}
+	}
+	
+	public static void createAvailabilitiesTable()  {
+		String sql = "CREATE TABLE IF NOT EXISTS EmployeeAvailabilities (Foreign Key(businessname) references Businessinfo(businessname), name Varchar(255), availability Varchar(255))";
+		try {
+			Connection c = getDBConnection();
+			Statement stmt = c.createStatement();
+	            stmt.execute(sql);
+		}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public static void createEmployeeWorkingTimesTable()  {
+		String sql = "CREATE TABLE IF NOT EXISTS EmployeeWorkingTimes (Foreign Key(businessname) references Businessinfo(businessname), name Varchar(255), shift Varchar(255))";
+		try {
+			Connection c = getDBConnection();
+			Statement stmt = c.createStatement();
+	            stmt.execute(sql);
+		}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public static void createBookingsTable()  {
+		String sql = "CREATE TABLE IF NOT EXISTS BookingsTable ( bookingId integer Primary Key, Foreign Key(businessname) references Businessinfo(businessname), username Varchar(255), bookingData Varchar(255)";
+		try {
+			Connection c = getDBConnection();
+			Statement stmt = c.createStatement();
+	            stmt.execute(sql);
+		}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
+		
 	
 	public static ResultSet getUserRow(String username) throws SQLException {
 		Connection c = getDBConnection();
