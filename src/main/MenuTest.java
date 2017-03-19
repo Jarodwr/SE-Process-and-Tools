9a9896caefdd7afc30ac28a21ce6519504a1ad1a
russellmenezes <s3600950@student.rmit.edu.au>
2017-03-19 12:25:16 +1100

    JUnit testing update

--- src/main/MenuTest.java
+++ src/main/MenuTest.java
@@ -5,17 +5,27 @@ package main;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.io.*;
+import java.util.Scanner;
+
+import org.junit.*;
 
 
 public class MenuTest {
 
+	Menu view = new Menu();
+	
+	@BeforeClass
+	public static void setup()
+	{
+		
+	}
+	
 	@Test
 	public void loginValidTest() {
 		
 		/*This check is also done during user registration*/
 		
-		Menu view = new Menu();
 		Boolean loginCheck = view.validatelogin("username","password");
 		
 		if (!loginCheck) {
@@ -28,12 +38,26 @@ public class MenuTest {
 		
 		/*This check is also done during user registration*/
 		
-		Menu view = new Menu();
 		Boolean loginCheck = view.validatelogin("username","!password");
 		
 		if (!loginCheck) {
 			fail("Login invalid character found.");
 		}
+	}
+	
+	@Test
+	public void UserLoginTest1()
+	{
+		String data = "Hello, World!\r\n";
+		InputStream stdin = System.in;
+		try {
+		  System.setIn(new ByteArrayInputStream(data.getBytes()));
+		  Scanner scanner = new Scanner(System.in);
+		  System.out.println(scanner.nextLine());
+		} finally {
+		  System.setIn(stdin);
+		}
+		
 	}
 
 }
