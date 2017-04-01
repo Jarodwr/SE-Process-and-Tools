2dbaa6555affc7a6d372affec19aa3745d0e9b7d
russellmenezes <s3600950@student.rmit.edu.au>
2017-04-01 18:06:19 +1100

    started GUI

---
+++ src/gui/login/LoginController.java
@@ -0,0 +1,29 @@
+package gui.login;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController 
+{
+	@FXML // fx:id="login_username"
+    private TextField login_username; // Value injected by FXMLLoader
+
+    @FXML // fx:id="login_password"
+    private PasswordField login_password; // Value injected by FXMLLoader
+
+    @FXML
+    void loginClick(ActionEvent event) 
+    {
+    	System.out.println(login_username.getText());
+    	System.out.println(login_password.getText());
+    }
+
+    @FXML
+    void loginSignupClick(ActionEvent event) 
+    {
+    	System.out.println("clicked the signup button");
+    	
+    }
+}
