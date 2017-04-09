909e3193a9adaf29d4ce5827a45c46aa40f54e44
Anesu Chiodze <s3542943@student.rmit.edu.au>
2017-04-10 02:43:41 +1000

    Some JUnit tests for Utility

--- src/model/utility/UtilityTest.java
+++ src/model/utility/UtilityTest.java
@@ -1,14 +1,42 @@
 package model.utility;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.instanceOf;
 
 import org.junit.Test;
 
-public class UtilityTest {
+import controller.Controller;
+import model.users.Customer;
+import model.users.Owner;
+import model.users.User;
 
+public class UtilityTest {
+	
+	static Controller c = new Controller();
+	static User user = new Owner("name", "pass", "SARJ's Milk Business", "Admin", "124 Address", "0412345678");
+	static Utility u = new Utility();
+
+	
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSerarchUser1() {
+		
+		User owner = u.searchUser("Ownertest123");
+		assert(owner == null);
 	}
+	
+	@Test
+	public void testSerarchUser2() {
+		
+		User owner = u.searchUser("Ownertest");
+		assertThat(owner, instanceOf(Owner.class));
+	}
+	
+	/*@Test
+	public void testSerarchUser3() {
+		
+		User owner = u.searchUser("name");
+		assertThat(owner, instanceOf(Customer.class));
+	}*/
 
 }
