2fbbf5248920491550278cd157b3a769d2ffa279
Anesu Chiodze <s3542943@student.rmit.edu.au>
2017-04-26 17:29:03 +1000

    Ability to view all employee working times regardless of the week. Will work on specific week after fixing some current bugs.

--- src/gui/owner/OwnerViewWorkingTimesController.java
+++ src/gui/owner/OwnerViewWorkingTimesController.java
@@ -2,25 +2,36 @@ package gui.owner;
 
 import java.net.URL;
 import java.text.SimpleDateFormat;
+
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
+
 import java.util.ResourceBundle;
 
 import controller.Controller;
-import javafx.beans.property.ReadOnlyIntegerWrapper;
-import javafx.beans.property.ReadOnlyStringWrapper;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.input.InputMethodEvent;
 import javafx.scene.input.MouseDragEvent;
 import model.employee.Employee;
 import model.period.Period;
+
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+
+import javafx.scene.control.TableColumn.CellDataFeatures;
+
+import javafx.util.Callback;
 
 public class OwnerViewWorkingTimesController {
 
@@ -46,38 +57,40 @@ public class OwnerViewWorkingTimesController {
 
     @FXML
     void generateTimetable(ActionEvent event) {
-    	int output = week.getSelectionModel().getSelectedIndex();
+    	int selectedWeek = week.getSelectionModel().getSelectedIndex(); // Get the index value of the selected week in the comboBox
+    	int selectedEmployeeIndex = employeeList.getSelectionModel().getSelectedIndex(); // Get the index value of the selected week in the comboBox
+    	String selectedEmployee = (String) employeeList.getValue();
+    	String employeeId = "";
     	
-    	if (output != -1) {
-    		insertEmployeeTimes(output); //Set columns dates of the selected week
-    		
-    		String[][] employeeWorkingTimes = this.c.getWorkingTimes("1");
-    		
-    		//String[] temp = {"1", "2", "T", "Wednesday", "Thursday", "Friday", "Saturday"};
+    	if (selectedEmployeeIndex != -1 && selectedWeek != -1) {
+    	
+    	int idSplit = selectedEmployee.indexOf(":");
 
-    		
-    		
-    		
-    		/*
-    		for (int i = tableView.getColumns().size(); i < values.length; i++) {
-                TableColumn<List<String>, String> col = new TableColumn<>("Column "+(i+1));
-                col.setMinWidth(80);
-                final int colIndex = i ;
-                col.setCellValueFactory(data -> {
-                    List<String> rowValues = data.getValue();
-                    String cellValue ;
-                    if (colIndex < rowValues.size()) {
-                        cellValue = rowValues.get(colIndex);
-                    } else {
-                         cellValue = "" ;
-                    }
-                    return new ReadOnlyStringWrapper(cellValue);
-                });
-                table.getColumns().add(col);
-            }
+    	if (idSplit != -1)
+    		employeeId = selectedEmployee.substring(0 , idSplit);
+    	
+    	String employeeName = selectedEmployee.substring(idSplit+1 ,selectedEmployee.length());
 
-            // add row:
-            table.getItems().add(Arrays.asList(values));*/
+    	if (idSplit != -1) { //Check if everything is fine before continuing
+    		insertEmployeeTimes(selectedWeek,employeeId,employeeName); //Set columns dates of the selected week
+    	}
+    	
+    	} else {
+    		
+    		//Inform the user of what they are missing
+    		
+    		if (selectedEmployeeIndex == -1) { // Invalid employee selected
+        		tableView.setPlaceholder(new Label("Please select an employee.")); //Notify the user of not selecting an employee
+        	}
+    		
+    		if (selectedWeek == -1) { //No week selected
+        		tableView.setPlaceholder(new Label("Please select a valid week.")); //Notify the user of not selecting a week
+        	}
+    		
+    		if (selectedWeek == -1 && selectedEmployeeIndex == -1) { // Nothing selected
+    			tableView.setPlaceholder(new Label("You need to both select an employee and the week of interest."));
+    		}
+    		
     		
     	}
     }
@@ -111,8 +124,11 @@ public class OwnerViewWorkingTimesController {
     {
     	this.c = c;
     	
+    	//Default table placeholder
+    	tableView.setPlaceholder(new Label("Please select an employee and the week of interest."));
+    	
     	//ONLY do this once
-    	this.c.addWorkingTime("0", "26/04/2017", "10:00", "11:00");
+    	this.c.addWorkingTime("0", "27/04/2017", "10:00", "11:00");
     	
     	if (Employees[0] != null) {
     		for (int i = 0; i < Employees.length; i++)
@@ -128,8 +144,49 @@ public class OwnerViewWorkingTimesController {
     }
     
     
-    public void insertEmployeeTimes(int weekNo) {
+    public void insertEmployeeTimes(int weekNo, String employeeID,String employeeName) {
     	
+    	
+        
+        tableView.getColumns().clear(); // Clear the columns
+
+        String[][] employeeWorkingTimes = this.c.getWorkingTimes(employeeID);
+        
+        System.out.println("Employee: "+employeeWorkingTimes);
+        
+        if (employeeWorkingTimes == null) {
+        	tableView.setPlaceholder(new Label(employeeName+" has no registered working times"));
+        } else {
+        
+        String [][] convertedWorkingTimes = getWeekDays(employeeWorkingTimes,this.c.Weekdays,weekNo);
+        
+        ObservableList<String[]> data = FXCollections.observableArrayList();
+        data.addAll(Arrays.asList(convertedWorkingTimes));
+        data.remove(0);//remove titles from data
+        for (int i = 0; i < convertedWorkingTimes[0].length; i++) {
+        	TableColumn tc = new TableColumn(convertedWorkingTimes[0][i]);
+        	
+        	final int colNo = i;
+        	tc.setCellValueFactory(new Callback<CellDataFeatures<String[], String>, ObservableValue<String>>() {
+        		@Override
+        		public ObservableValue<String> call(CellDataFeatures<String[], String> p) {
+        			return new SimpleStringProperty((p.getValue()[colNo]));
+        			}
+        		});
+        	tc.setPrefWidth(120);
+        	tableView.getColumns().add(tc);
+}
+        tableView.setItems(data);
+        }
+        
+
+    }
+    
+    
+    
+    
+    public String[][] getWeekDays(String[][] contents,String[] Weekdays,int weekNo) {
+    	int[] DayPeriodCounts = new int[Weekdays.length]; //Used to figure out the number of rows in the table
     	Date date = new Date();
         Calendar cl = Calendar.getInstance();
         cl.setTime(date);
@@ -147,84 +204,6 @@ public class OwnerViewWorkingTimesController {
         
         Date currentDate; // Will store current date being processed
         SimpleDateFormat weekDate = new SimpleDateFormat("d/M"); // Format for the week dates
-        
-        tableView.getColumns().clear(); // Clear the columns
-    	
-        List<String> stringValues = Arrays.asList("One", "Two", "Three", "Four", "Five");
-        
-        for (int i = 0; i < stringValues.size(); i++) {
-            tableView.getItems().add(i);
-        }
-        
-        
-        
-        String[][] employeeWorkingTimes = this.c.getWorkingTimes("0");
-        String currentTime;
-        
-        String [][] convertedWorkingTimes = getWeekDays(employeeWorkingTimes,this.c.Weekdays);
-        
-        if (employeeWorkingTimes != null)
-        	System.out.println(employeeWorkingTimes[0][0]);
-    	
-    	for (int i = 0; i <  convertedWorkingTimes.length; i++) { //this.c.Weekdays.length
-    		
-    		
-    		
-    		if (i != 0) {
-    			cl.add(Calendar.DATE, 1);
-    		}
-    		
-    		
-    		currentDate = cl.getTime();
-    		
-    		
-    		
-    		for (int j = 0; j < convertedWorkingTimes[i].length; j++) {
-    			
-    			//employeeWorkingTimes[i].length
-    		
-    		
-    			
-    			System.out.println("TEST: "+convertedWorkingTimes[i].length);
-    			
-
-    			System.out.println("TEST: "+convertedWorkingTimes[0][j]);
-    			
-    			TableColumn<Integer, String> TableColumn = new TableColumn<>(this.c.Weekdays[j]+" "+weekDate.format(currentDate));
-    			
-    		TableColumn.setCellValueFactory(cellData -> {
-                Integer rowIndex = cellData.getValue();
-                return new ReadOnlyStringWrapper(convertedWorkingTimes[0][rowIndex]); //stringValues.get(rowIndex)
-            });
-    		
-    		tableView.getColumns().add(TableColumn);
-    	}
-    		//formatDateToTime(employeeWorkingTimes[0][0])
-            
-            
-    		//}
-    		//tempColumn.prefWidthProperty().bind(tableView.widthProperty().multiply(0.14));
-            //TableColumn.setMinWidth(80); // Might use this later
-    	}
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    }
-    
-    public static String formatDateToTime(String unixdate) {
-		Long date = Long.parseLong(unixdate) * 1000;
-		SimpleDateFormat viewing = new SimpleDateFormat("h:mm");
-		
-		return viewing.format(date);
-}
-    
-    
-    public String[][] getWeekDays(String[][] contents,String[] Weekdays) {
-int[] DayPeriodCounts = new int[Weekdays.length]; //Used to figure out the number of rows in the table
 		
 		for (int i=0;i < DayPeriodCounts.length; i++) {
 			DayPeriodCounts[i] = 0; //initialize all the days to 0 count of working periods
@@ -234,8 +213,8 @@ int[] DayPeriodCounts = new int[Weekdays.length]; //Used to figure out the numbe
 		int dayWithMostWorkingHours = 0;
 		
 		for (int i=0;i < contents.length; i++) { //Find out the maximum number of working hours/period per day
-			
-					ConvertedDay = Period.convertSecondsToDay((int)(Long.parseLong(contents[i][0]) /1000)); //Convert to day from milliseconds to seconds then day
+			///1000
+					ConvertedDay = Period.convertSecondsToDay((int)(Long.parseLong(contents[i][0])  )); //Convert to day from milliseconds to seconds then day
 					ConvertedDay = Period.convertSecondsToDay((int)(Long.parseLong(contents[i][0]))); //Convert to day from milliseconds to seconds then day
 					DayPeriodCounts[Arrays.asList(Weekdays).indexOf(ConvertedDay)] += 1; //increase the specific day's count of working periods
 					
@@ -247,14 +226,29 @@ int[] DayPeriodCounts = new int[Weekdays.length]; //Used to figure out the numbe
 		
 		
 		
-		String[][] convertedDates = new String[dayWithMostWorkingHours][Weekdays.length]; //Array that will be used to print out the table of the weekly view
+		String[][] convertedDates = new String[dayWithMostWorkingHours+1][Weekdays.length]; //Array that will be used to print out the table of the weekly view
+		
+		//Add weeks
+		for (int i = 0; i < Weekdays.length; i++) { 
+			if (i != 0) {
+    			cl.add(Calendar.DATE, 1);
+    		}
+    		
+    		
+    		currentDate = cl.getTime();
+			convertedDates[0][i] = Weekdays[i]+" "+weekDate.format(currentDate);
+		}
+		
+		
+		
+		
 		
 		for (int i=0;i < contents.length; i++) {
 			
 			ConvertedDay = Period.convertSecondsToDay((int)(Long.parseLong(contents[i][0])));
 			
 			/* Add it to the table under the specific day column with start - end time 24 hr format*/
-			for (int j = 0; j < contents[i].length; j++) { 
+			for (int j = 1; j < contents[i].length; j++) { 
 				if (convertedDates[j][Arrays.asList(Weekdays).indexOf(ConvertedDay)] == null) {
 					convertedDates[j][Arrays.asList(Weekdays).indexOf(ConvertedDay)] = Period.get24HrTimeFromWeekTime(contents[i][0])+" - "+Period.get24HrTimeFromWeekTime(contents[i][1]);
 					break;
@@ -271,6 +265,55 @@ int[] DayPeriodCounts = new int[Weekdays.length]; //Used to figure out the numbe
 			
 		}
     }
+		
+		//Menu Title			
+				System.out.println("\n--------------------\nEmployee Availability\n--------------------\n");
+
+				
+				/*Table header*/
+				String tableTitles = "";
+				
+				for (int i = 0; i < Weekdays.length;i++) {
+					if (i == 0)
+						tableTitles += "     "+Weekdays[i]; // First table header title
+					else
+						tableTitles += "   | "+Weekdays[i]; // Second or more element of the table header title
+					
+					for (int k = 0; k < (12-Weekdays[i].length()); k++)
+						tableTitles += " ";
+					
+				
+				}
+				System.out.print(tableTitles); //Print all the header titles
+				
+				//Close header
+				System.out.println(); 
+				for (int k = 0; k < (tableTitles.length()); k++)
+					System.out.print("-"); //Line below the headers
+				
+				
+				System.out.println(); //Create a new line for the rest of the table contents
+				
+				
+						for (int i=1;i < convertedDates.length; i++) { //Go through all the rows
+							for (int j = 0; j < convertedDates[i].length; j++) { //Go through all the columns
+								
+								if (j ==0)
+									System.out.print("   ");
+								
+								System.out.print("  "+convertedDates[i][j]+"   "); //Print out the current detail
+								
+								/*Add enough spaces to keep table column length balanced*/
+								
+								if ( convertedDates[i][j] == "")
+									for (int k = 0; k < 12; k++)
+										System.out.print(" "); //Create a gap between columns
+							}
+							System.out.println(); //create a new row
+						}
+						
+						for (int k = 0; k < (tableTitles.length()); k++)
+							System.out.print("-"); //Add dashes "-" under the table
 		
 		
 		
