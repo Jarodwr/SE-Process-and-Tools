dc7e91cbd11ceeb2b785e3ed39e77b0ffbd3f4e9
jarodwr <wrjarod@gmail.com>
2017-04-28 02:34:36 +1000

    Split time picker into its own class

--- src/gui/owner/OwnerAddBooking.java
+++ src/gui/owner/OwnerAddBooking.java
@@ -1,5 +1,6 @@
 package gui.owner;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
@@ -11,6 +12,7 @@ import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckMenuItem;
@@ -36,12 +38,13 @@ public class OwnerAddBooking {
    
     @FXML
     private MenuButton serviceMenu;
-    
-    @FXML
-    private GridPane timeGrid;
 
     @FXML
     private Button AddBookingBtn;
+    
+    @FXML
+    private Pane timeMenu;
+    private TimePicker time;
 
     private Controller controller;
     
@@ -51,12 +54,6 @@ public class OwnerAddBooking {
     private ArrayList<String> services = new ArrayList<String>();
     
     private LocalDate date = null;
-    
-    private String[] availableStyle = new String[]{"-fx-background-color: #ffffff","-fx-background-color: #dddddd", "-fx-background-color: #bbbbbb"};
-    private String[] unavailableStyle = new String[]{"-fx-background-color: #ff0000","-fx-background-color: #dd0000", "-fx-background-color: #bb0000"};
-    
-    private ArrayList<Integer> selected = new ArrayList<Integer>();
-    private ArrayList<Integer> available = new ArrayList<Integer>();
     
     private int duration = 1;
 
@@ -82,37 +79,11 @@ public class OwnerAddBooking {
     	//Update gridtable with availabilities
     	
     	if (date != null && employeeId != null) {
-        	
-        	long dayInMillis = 86400000;
-        	long periodInMillis = 1800000;
-        	
+    		long dayInMillis = 86400000;
         	String[][] times = controller.getEmployeeBookingAvailability(employeeId, new Date(date.toEpochDay() * dayInMillis));
-        	if (times != null && times.length > 0) {
-        		
-            	for (int i : available)	//Reset all available panes to unavailable panes
-            		getTimePane(i).setStyle(unavailableStyle[0]);
-            	available.removeAll(available);	//Wipe info from panes
+        	if (times != null && times.length > 0)
+        		time.setAvailability(times, date);
 
-            	
-            	for (long startTime = date.toEpochDay() * dayInMillis; startTime < (date.toEpochDay()+1) * dayInMillis; startTime += periodInMillis) {
-        			Long endTime = startTime + periodInMillis;
-        			
-        			boolean success = true;
-            		for (String[] p : times) {
-            			if (Long.parseLong(p[0]) < startTime && Long.parseLong(p[1]) > startTime
-            					|| Long.parseLong(p[0]) < endTime && Long.parseLong(p[0]) > endTime) {
-            				success = false;
-            				break;
-            			}
-            		}
-    				if (success)
-    					available.add((int)((startTime - date.toEpochDay() * dayInMillis)/periodInMillis));
-            	}
-            	
-            	for (int i : available)	//Paint available panes white
-            		getTimePane(i).setStyle(availableStyle[0]);;
-            	
-        	}
 
     	}
     	
@@ -123,8 +94,8 @@ public class OwnerAddBooking {
     @FXML
     void addBooking(ActionEvent event) {
     	int localStart = 0;
-    	for (int i : selected)
-    		if (i > localStart)
+    	for (int i : time.getSelectedPeriods())
+    		if (i < localStart)
     			localStart = i;
 
     	long startTime = date.toEpochDay() * 86400000 + localStart * 1800000;
@@ -147,24 +118,18 @@ public class OwnerAddBooking {
     		CheckMenuItem cmi = new CheckMenuItem(s);
     		serviceMenu.getItems().add(cmi);
     	}
-    	
-    	//Time grid init
-    	for (int i = 0; i < 24; i++) {
-    		Pane am = new Pane();
-    		am.setStyle(unavailableStyle[0]);
-    		addPaneListener(am, i);
-    		timeGrid.add(am, i, 0);
-    		
-    		Pane pm = new Pane();
-    		pm.setStyle(unavailableStyle[0]);
-    		addPaneListener(pm, i+24);
-    		timeGrid.add(pm, i, 2);
-    		
-    		if (i%2 == 0) {
-    			timeGrid.add(new Label(Integer.toString(i/2)), i, 1);
-    			timeGrid.add(new Label(Integer.toString((i + 24)/2)), i, 3);
-    		}
-    	}
+    	//Alternate time grid init
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("TimePicker.fxml"));
+			System.out.println(timeMenu);
+			timeMenu.getChildren().clear();
+			timeMenu.getChildren().add(loader.load());
+			
+			time = loader.getController();
+			time.init(timeMenu);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     	
     	serviceMenu.showingProperty().addListener(new ChangeListener<Boolean>() {
             @Override
@@ -197,74 +162,9 @@ public class OwnerAddBooking {
             	if (duration == 0)
             		duration = 1;
 
+            	time.setDuration(duration);
             	update();
             }
         });
-    }
-    
-    private void addPaneListener(Pane p, int i) {
-    	p.onMouseClickedProperty().set(new EventHandler<MouseEvent>(){
-			@Override
-			public void handle(MouseEvent arg0) {
-				String[] style = getAppropriateStyle(i);
-				p.setStyle(style[2]);
-				
-				for (int j : selected) {
-					getTimePane(j).setStyle(style[0]);
-				}
-
-				selected.removeAll(selected);
-				for (int j = i; j < Math.min(i + duration, 48); j++) {
-					selected.add(j);
-					getTimePane(j).setStyle(style[2]);
-				}
-			}
-    	});
-    	
-    	p.onMouseEnteredProperty().set(new EventHandler<MouseEvent>(){
-			@Override
-			public void handle(MouseEvent arg0) {
-				String[] style = getAppropriateStyle(i);
-				for (int j = i; j < Math.min(i + duration, 48); j++)
-					if (!selected.contains(j))
-						getTimePane(j).setStyle(style[1]);
-			}
-    	});
-    	
-    	p.onMouseExitedProperty().set(new EventHandler<MouseEvent>(){
-			@Override
-			public void handle(MouseEvent arg0) {
-				String[] style = getAppropriateStyle(i);
-				for (int j = i; j < Math.min(i + duration, 48); j++)
-					if (!selected.contains(j))
-						getTimePane(j).setStyle(style[0]);
-			}
-    	});
-    }
-    
-    private Pane getTimePane(int i) {
-    	int row, col;
-    	if (i < 24) {
-    		row = 0;
-    		col = i;
-    	} else if (i < 48) {
-    		row = 2;
-    		col = i-24;
-    	} else {
-    		return null;
-    	}
-
-    	for (Node node : timeGrid.getChildren())
-            if (GridPane.getColumnIndex(node) == col && GridPane.getRowIndex(node) == row)
-                return (Pane)node;
-
-        return null;
-    }
-    
-    private String[] getAppropriateStyle(int i) {
-    	for (int a : available)
-    		if (a == i)
-    			return availableStyle;
-    	return unavailableStyle;
     }
 }
