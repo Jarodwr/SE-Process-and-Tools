1d7428f8c9e25ca05ea1e4f8bd7e46ad54844315
jarodwr <wrjarod@gmail.com>
2017-05-05 02:07:03 +1000

    select multiple working time units at once

--- src/gui/owner/WorkingTimesPicker.java
+++ src/gui/owner/WorkingTimesPicker.java
@@ -3,11 +3,14 @@ package gui.owner;
 import java.time.LocalDate;
 
 import javafx.event.EventHandler;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import model.period.Period;
 
 public class WorkingTimesPicker extends TimePicker {
+	
+	int focus = -1;
 	
     @Override
     public void init(Pane p) {
@@ -83,16 +86,55 @@ public class WorkingTimesPicker extends TimePicker {
     
     @Override
     protected void addPaneListener(Pane p, int i) {
+    	
     	p.onMouseClickedProperty().set(new EventHandler<MouseEvent>(){
 			@Override
 			public void handle(MouseEvent arg0) {
-				
-				if (selected.contains(i)) {
-					selected.remove(Integer.valueOf(i));
-					p.setStyle(getAppropriateStyle(i)[0]);
+				if (focus > -1 && focus != i) {
+					if (focus > i) {
+						for (int j = i; j <= focus; j++) {
+							if (arg0.getButton() == MouseButton.SECONDARY) {
+								selected.remove(Integer.valueOf(j));
+								getTimePane(j).setStyle(getAppropriateStyle(j)[0]);
+							} else if (arg0.getButton() == MouseButton.PRIMARY){
+								if (selected.contains(j))
+									continue;
+								selected.add(j);
+								getTimePane(j).setStyle(getAppropriateStyle(j)[2]);
+							}
+							
+						}
+					} else {
+						for (int j = i; j >= focus; j--) {
+							if (arg0.getButton() == MouseButton.SECONDARY) {
+								selected.remove(Integer.valueOf(j));
+								getTimePane(j).setStyle(getAppropriateStyle(j)[0]);
+							} else if (arg0.getButton() == MouseButton.PRIMARY){
+								if (selected.contains(j))
+									continue;
+								selected.add(j);
+								getTimePane(j).setStyle(getAppropriateStyle(j)[2]);
+							}
+							
+						}
+					}
+					focus = -1;
 				} else {
-					selected.add(i);
-					p.setStyle(getAppropriateStyle(i)[2]);
+					if (focus == i) {
+						if (arg0.getButton() == MouseButton.SECONDARY) {
+							if (selected.contains(i))
+								selected.remove(Integer.valueOf(i));
+						} else if (arg0.getButton() == MouseButton.PRIMARY) {
+							if (selected.contains(i))
+								return;
+							selected.add(i);
+						}
+						p.setStyle(getAppropriateStyle(i)[2]);
+
+						focus = -1;
+					} else {
+						focus = i;
+					}
 				}
 			}
     	});
@@ -100,18 +142,52 @@ public class WorkingTimesPicker extends TimePicker {
     	p.onMouseEnteredProperty().set(new EventHandler<MouseEvent>(){
 			@Override
 			public void handle(MouseEvent arg0) {
-				if (selected.contains(i))
-					return;
-				p.setStyle(getAppropriateStyle(i)[1]);
+
+				if (focus > -1) {
+					if (focus > i) {
+						for (int j = i; j < focus; j++) {
+							if (selected.contains(j))
+								continue;
+							getTimePane(j).setStyle(getAppropriateStyle(j)[1]);
+						}
+					} else {
+						for (int j = i; j > focus; j--) {
+							if (selected.contains(j))
+								continue;
+							getTimePane(j).setStyle(getAppropriateStyle(j)[1]);
+						}
+					}
+				} else {
+					if (selected.contains(i))
+						return;
+					p.setStyle(getAppropriateStyle(i)[1]);
+				}
 			}
     	});
     	
     	p.onMouseExitedProperty().set(new EventHandler<MouseEvent>(){
 			@Override
 			public void handle(MouseEvent arg0) {
-				if (selected.contains(i))
-					return;
-				p.setStyle(getAppropriateStyle(i)[0]);
+
+				if (focus > -1) {
+					if (focus > i) {
+						for (int j = i; j < focus; j++) {
+							if (selected.contains(j))
+								continue;
+							getTimePane(j).setStyle(getAppropriateStyle(j)[0]);
+						}
+					} else {
+						for (int j = i; j > focus; j--) {
+							if (selected.contains(j))
+								continue;
+							getTimePane(j).setStyle(getAppropriateStyle(j)[0]);
+						}
+					}
+				} else {
+					if (selected.contains(i))
+						return;
+					p.setStyle(getAppropriateStyle(i)[0]);
+				}
 			}
     	});
     }
