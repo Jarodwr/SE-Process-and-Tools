572e8482e0bfed53ff55bcef003a8c71a15c99de
jarodwr <wrjarod@gmail.com>
2017-05-05 02:24:28 +1000

    remove some system prints

--- src/gui/owner/WorkingTimesPicker.java
+++ src/gui/owner/WorkingTimesPicker.java
@@ -63,7 +63,7 @@ public class WorkingTimesPicker extends TimePicker {
     	available.removeAll(available);	//Wipe info from panes
 
     	int day = (int)(((date.toEpochDay() * dayInSecs) / 86400) % 7);
-    	System.out.println(day);
+
     	for (long startTime = day * dayInSecs; startTime < (day+1) * dayInSecs; startTime += periodInSecs) {
 			Long endTime = startTime + periodInSecs;
 			boolean success = false;
@@ -193,7 +193,7 @@ public class WorkingTimesPicker extends TimePicker {
     }
     
     public void setDefaultAvailability(String[][] times, String day) {
-    	System.out.println(day);
+
     	int lowerBoundsDay = ((Integer.parseInt(day)-4)%7 )* 86400;
     	int upperBoundsDay = lowerBoundsDay + 86400;
     	
@@ -216,7 +216,6 @@ public class WorkingTimesPicker extends TimePicker {
     		time1 = (time1 / (30*60))%48;
     		time2 = (time2 / (30*60))%48;
     		
-    		System.out.println(time1 + ":" + time2);
     		for(int j = time1; j <= time2; j++) {
     			available.add(j);
     			getTimePane(j).setStyle(availableStyle[0]);
