2005ca45bbc0ebc3f6834d98d52688b1b8ebd10c
jarodwr <wrjarod@gmail.com>
2017-05-07 17:55:30 +1000

    test template for utility test

--- src/model/utility/UtilityTest.java
+++ src/model/utility/UtilityTest.java
@@ -1,10 +1,13 @@
 package model.utility;
 
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.instanceOf;
+
+import org.junit.AfterClass;
+import org.junit.Before;
 
 import org.junit.Test;
 
+import model.users.Customer;
 import model.users.Owner;
 import model.users.User;
 
@@ -15,27 +18,658 @@ public class UtilityTest {
 	static User user = new Owner("name", "pass", "SARJ's Milk Business", "Admin", "124 Address", "0412345678");
 	static Utility u = new Utility();
 
-	
-	
-	@Test
-	public void testSerarchUser1() {
+	@Before
+	public void refresh() {
 		
-		User owner = u.searchUser("Ownertest123");
-		assert(owner == null);
+	}
+	
+	@AfterClass
+	public void changeBack() {
+		
 	}
 	
 	@Test
-	public void testSerarchUser2() {
-		
-		User owner = u.searchUser("Ownertest");
-		assertThat(owner, instanceOf(Owner.class));
-	}
-	
-	/*@Test
-	public void testSerarchUser3() {
-		
-		User owner = u.searchUser("name");
-		assertThat(owner, instanceOf(Customer.class));
-	}*/
+	public void searchUser1() {
 
+	}
+	
+	@Test
+	public void searchUser2() {
+
+	}
+	
+	@Test
+	public void searchUser3() {
+
+	}
+	
+	@Test
+	public void searchUser4() {
+
+	}
+	
+	@Test
+	public void searchUser5() {
+
+	}
+	
+	@Test
+	public void searchUser6() {
+
+	}
+	
+	
+	@Test
+	public void authenticate1() {
+		
+	}
+	
+	@Test
+	public void authenticate2() {
+		
+	}
+	
+	@Test
+	public void authenticate3() {
+		
+	}
+	
+	@Test
+	public void authenticate4() {
+		
+	}
+	
+	@Test
+	public void authenticate5() {
+		
+	}
+	
+	@Test
+	public void authenticate6() {
+		
+	}
+
+	
+	@Test
+	public void getEmployeeList1() {
+		
+	}
+	
+	@Test
+	public void getEmployeeList2() {
+		
+	}
+	
+	@Test
+	public void getEmployeeList3() {
+		
+	}
+	
+	@Test
+	public void getEmployeeList4() {
+		
+	}
+	
+	@Test
+	public void getEmployeeList5() {
+		
+	}
+	
+	@Test
+	public void getEmployeeList6() {
+		
+	}
+	
+	
+	@Test
+	public void getAvailableTimes1() {
+		
+	}
+	
+	@Test
+	public void getAvailableTimes2() {
+		
+	}
+	
+	@Test
+	public void getAvailableTimes3() {
+		
+	}
+	
+	@Test
+	public void getAvailableTimes4() {
+		
+	}
+	
+	@Test
+	public void getAvailableTimes5() {
+		
+	}
+	
+	@Test
+	public void getAvailableTimes6() {
+		
+	}
+	
+	
+	@Test
+	public void getApplicableEmployees1() {
+		
+	}
+	
+	@Test
+	public void getApplicableEmployees2() {
+		
+	}
+	
+	@Test
+	public void getApplicableEmployees3() {
+		
+	}
+	
+	@Test
+	public void getApplicableEmployees4() {
+		
+	}
+	
+	@Test
+	public void getApplicableEmployees5() {
+		
+	}
+	
+	@Test
+	public void getApplicableEmployees6() {
+		
+	}
+	
+	@Test
+	public void getAvailableBookingTimesByDuration1() {
+		
+	}
+	
+	@Test
+	public void getAvailableBookingTimesByDuration2() {
+		
+	}
+	
+	@Test
+	public void getAvailableBookingTimesByDuration3() {
+		
+	}
+	
+	@Test
+	public void getAvailableBookingTimesByDuration4() {
+		
+	}
+	
+	@Test
+	public void getAvailableBookingTimesByDuration5() {
+		
+	}
+	
+	@Test
+	public void getAvailableBookingTimesByDuration6() {
+		
+	}
+	
+	
+	@Test
+	public void getAvailableBookingTimes1() {
+		
+	}
+	
+	@Test
+	public void getAvailableBookingTimes2() {
+		
+	}
+	
+	@Test
+	public void getAvailableBookingTimes3() {
+		
+	}
+	
+	@Test
+	public void getAvailableBookingTimes4() {
+		
+	}
+	
+	@Test
+	public void getAvailableBookingTimes5() {
+		
+	}
+	
+	@Test
+	public void getAvailableBookingTimes6() {
+		
+	}
+	
+	
+	@Test
+	public void getBookingsAfter1() {
+		
+	}
+	
+	@Test
+	public void getBookingsAfter2() {
+		
+	}
+	
+	@Test
+	public void getBookingsAfter3() {
+		
+	}
+	
+	@Test
+	public void getBookingsAfter4() {
+		
+	}
+	
+	@Test
+	public void getBookingsAfter5() {
+		
+	}
+	
+	@Test
+	public void getBookingsAfter6() {
+		
+	}
+	
+	
+	@Test
+	public void addNewBooking1() {
+		
+	}
+	
+	@Test
+	public void addNewBooking2() {
+		
+	}
+	
+	@Test
+	public void addNewBooking3() {
+		
+	}
+	
+	@Test
+	public void addNewBooking4() {
+		
+	}
+	
+	@Test
+	public void addNewBooking5() {
+		
+	}
+	
+	@Test
+	public void addNewBooking6() {
+		
+	}
+	
+	@Test
+	public void removeBooking1() {
+		
+	}
+	
+	@Test
+	public void removeBooking2() {
+		
+	}
+	
+	@Test
+	public void removeBooking3() {
+		
+	}
+	
+	@Test
+	public void removeBooking4() {
+		
+	}
+	
+	@Test
+	public void removeBooking5() {
+		
+	}
+	
+	@Test
+	public void removeBooking6() {
+		
+	}
+	
+	@Test
+	public void getShift1() {
+		
+	}
+	
+	@Test
+	public void getShift2() {
+		
+	}
+	
+	@Test
+	public void getShift3() {
+		
+	}
+	
+	@Test
+	public void getShift4() {
+		
+	}
+	
+	@Test
+	public void getShift5() {
+		
+	}
+	
+	@Test
+	public void getShift6() {
+		
+	}
+	
+	
+	@Test
+	public void addShift1() {
+		
+	}
+	
+	@Test
+	public void addShift2() {
+		
+	}
+	
+	@Test
+	public void addShift3() {
+		
+	}
+	
+	@Test
+	public void addShift4() {
+		
+	}
+	
+	@Test
+	public void addShift5() {
+		
+	}
+	
+	@Test
+	public void addShift6() {
+		
+	}
+	
+	
+	@Test
+	public void removeShift1() {
+		
+	}
+	
+	@Test
+	public void removeShift2() {
+		
+	}
+	
+	@Test
+	public void removeShift3() {
+		
+	}
+	
+	@Test
+	public void removeShift4() {
+		
+	}
+	
+	@Test
+	public void removeShift5() {
+		
+	}
+	
+	@Test
+	public void removeShift6() {
+		
+	}
+	
+	
+	@Test
+	public void addCustomer1() {
+		
+	}
+	
+	@Test
+	public void addCustomer2() {
+		
+	}
+	
+	@Test
+	public void addCustomer3() {
+		
+	}
+	
+	@Test
+	public void addCustomer4() {
+		
+	}
+	
+	@Test
+	public void addCustomer5() {
+		
+	}
+	
+	@Test
+	public void addCustomer6() {
+		
+	}
+	
+	@Test
+	public void addEmployee1() {
+		
+	}
+	
+	@Test
+	public void addEmployee2() {
+		
+	}
+	
+	@Test
+	public void addEmployee3() {
+		
+	}
+	
+	@Test
+	public void addEmployee4() {
+		
+	}
+	
+	@Test
+	public void addEmployee5() {
+		
+	}
+	
+	@Test
+	public void addEmployee6() {
+		
+	}
+	
+	@Test
+	public void getAllEmployees1() {
+		
+	}
+	
+	@Test
+	public void getAllEmployees2() {
+		
+	}
+	
+	@Test
+	public void getAllEmployees3() {
+		
+	}
+	
+	@Test
+	public void getAllEmployees4() {
+		
+	}
+	
+	@Test
+	public void getAllEmployees5() {
+		
+	}
+	
+	@Test
+	public void getAllEmployees6() {
+		
+	}
+	
+	
+	@Test
+	public void getEmployeeAvailability1() {
+		
+	}
+	
+	@Test
+	public void getEmployeeAvailability2() {
+		
+	}
+	
+	@Test
+	public void getEmployeeAvailability3() {
+		
+	}
+	
+	@Test
+	public void getEmployeeAvailability4() {
+		
+	}
+	
+	@Test
+	public void getEmployeeAvailability5() {
+		
+	}
+	
+	@Test
+	public void getEmployeeAvailability6() {
+		
+	}
+	
+	
+	@Test
+	public void getAllCustomers1() {
+		
+	}
+	
+	@Test
+	public void getAllCustomers2() {
+		
+	}
+	
+	@Test
+	public void getAllCustomers3() {
+		
+	}
+	
+	@Test
+	public void getAllCustomers4() {
+		
+	}
+	
+	@Test
+	public void getAllCustomers5() {
+		
+	}
+	
+	@Test
+	public void getAllCustomers6() {
+		
+	}
+	
+	
+	@Test
+	public void getWorkingTimes1() {
+		
+	}
+	
+	@Test
+	public void getWorkingTimes2() {
+		
+	}
+	
+	@Test
+	public void getWorkingTimes3() {
+		
+	}
+	
+	@Test
+	public void getWorkingTimes4() {
+		
+	}
+	
+	@Test
+	public void getWorkingTimes5() {
+		
+	}
+	
+	@Test
+	public void getWorkingTimes6() {
+		
+	}
+	
+	
+	@Test
+	public void getAllServices1() {
+		
+	}
+	
+	@Test
+	public void getAllServices2() {
+		
+	}
+	
+	@Test
+	public void getAllServices3() {
+		
+	}
+	
+	@Test
+	public void getAllServices4() {
+		
+	}
+	
+	@Test
+	public void getAllServices5() {
+		
+	}
+	
+	@Test
+	public void getAllServices6() {
+		
+	}
+	
+	
+	@Test
+	public void getEmployeeBookingAvailability1() {
+		
+	}
+	
+	@Test
+	public void getEmployeeBookingAvailability2() {
+		
+	}
+	
+	@Test
+	public void getEmployeeBookingAvailability3() {
+		
+	}
+	
+	@Test
+	public void getEmployeeBookingAvailability4() {
+		
+	}
+	
+	@Test
+	public void getEmployeeBookingAvailability5() {
+		
+	}
+	
+	@Test
+	public void getEmployeeBookingAvailability6() {
+		
+	}
 }
