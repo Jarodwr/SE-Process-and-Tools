3da43436051a6b0152f5a09e417349a363b6549a
Anesu Chiodze <s3542943@student.rmit.edu.au>
2017-05-22 02:16:42 +1000

    remove todo in login controller, not needed

--- src/main/java/SARJ/BookingSystem/gui/CustomerViewController.java
+++ src/main/java/SARJ/BookingSystem/gui/CustomerViewController.java
@@ -47,6 +47,11 @@ public class CustomerViewController {
     private Controller c;
     
     private Stage main;
+    
+    /**
+	 * Add booking button pressed
+	 * @param event	the event information of the button press
+	 */
 
     @FXML
     void addBooking(ActionEvent event) {
@@ -63,6 +68,12 @@ public class CustomerViewController {
     		e.printStackTrace();
     	}
     }
+    
+    
+    /**
+	 * View booking button pressed
+	 * @param event	the event information of the button press
+	 */
 
     @FXML
     void viewBookings(ActionEvent event) {
@@ -80,12 +91,18 @@ public class CustomerViewController {
     	}
     }
     
+    
+    /**
+	 * Log the user out on logout button press
+	 * @param event	the event information of the button press
+	 */
+    
     @FXML
     void logout(ActionEvent event) 
     {
     	try 
     	{
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml")); /* TODO replace with page that says "are you sure you want to log out?" */
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml"));
 			BorderPane root = loader.load();
 	        Scene scene = new Scene(root, 900, 600);
 	        LoginController controller = loader.getController();
@@ -98,6 +115,10 @@ public class CustomerViewController {
 			e.printStackTrace();
 		}
     }
+    
+    /**
+	 * Initialize fxml object rules for the GUI
+	 */
 
     @FXML
     void initialize() {
@@ -108,12 +129,24 @@ public class CustomerViewController {
 
     }
     
+    /**
+	 * Initialize and setup the variables for the view
+	 * @param main	the GUI fxaml main stage
+	 * @param c	cthe controller object
+	 * @param u the current user object
+	 */
+    
     public void init(Stage main, Controller c, User u) 
     {
     	this.main = main;
     	customer = (Customer) u;
     	this.c = c;
 	}
+    
+    /**
+	 * Change colour of the selected option for visual feedback on selection
+	 * @param button	button/option selected
+	 */
     
     @FXML
     private void selectButton(Button selected) {
